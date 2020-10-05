a908810c2ca4ea27089575f58bdd2c8c8db72aa1
Berezhnyj <berezhnyj95@gmail.com>
2020-10-05 20:05:41 +0300

    Fix bugs

--- include/handlers.h
+++ include/handlers.h
@@ -1,3 +1,12 @@
+// Include Libraries
+#include <Wire.h>
+#include <ESP8266HTTPClient.h>
+
+uint32_t    timer_start;
+uint32_t    timer_stop;
+
+void set_timer_start();
+float set_timer_stop();
 
 void print_cli_start_message() {
     Serial.println("//--------------------------------------------------------------//");
@@ -7,6 +16,25 @@ void print_cli_start_message() {
     Serial.println("//--------------------------- START ----------------------------//");
 }
 
+/* Start timer */
+void set_timer_start() {
+  timer_stop = 0;
+  timer_start = micros();
+}
+/* Stop timer */
+float set_timer_stop() {
+  timer_stop = micros();
+  float time_delta = ((float)timer_stop - (float)timer_start) / 1000;
+  if(time_delta >= 60){
+    return (time_delta/60);
+  } else {
+    return (time_delta);
+  }
+}
+
+void debugPrintMessage(char *code, char *message) {
+    Serial.println((String)"");
+}
 /*
 static httpd_handle_t start_webserver(void)
 {
--- src/main.cpp
+++ src/main.cpp
@@ -15,36 +15,33 @@
 #include <Adafruit_Sensor.h>
 #include <ModbusTCPSlave.h>
 #include <coap-simple.h>
-
 #include "Adafruit_BME280.h"
+#include "handlers.h"
 /*----------------------------Підключення до Wifi мережі---------------------------*/
 /* Global variables and defines */
-#ifndef STASSID
-  #define STASSID "OzzyHome_2G"
-  #define STAPSK  "19951995"
-#endif
 #ifndef COAP
   #define COAP 0
   #define LEDP 9
 #endif
-#ifndef OTAUPDATE
-  #define OTAUPDATE true
-#endif
 #ifndef MODBUS232
-  #define MODBUS232 0  // Define flag to enable ModeBus
+  #define MODBUS232 0     // Define flag to enable ModeBus
   #define address   1     // Define one address for reading
   #define bitQty    1     // Define the number of bits to read
 #endif
+#define OTAUPDATE true  
+#define STASSID "OzzyHome_2G"
+#define STAPSK  "19951995"
 #define DEVICE_DELAY   100000  // Setup delay
 #define DHTPIN  2       // DHT22 connect to pin 0
 #define LED     D0      // Led in NodeMCU at pin GPIO16 (D0).
 #define SEC     1000    // One Sec define
-#define mSEC    100    // One mSec define
+#define mSEC    100     // One mSec define
 #define SEALEVELPRESSURE_HPA (1013.25)
 /* Object initialization */
 IPAddress   ESP_ip_addr;
 IPAddress   ESP_subnet_addr;
 IPAddress   ESP_gateway_addr;
+IPAddress dev_ip(0,0,0,0);
 /* Object initialization */
 const char* ssid         = STASSID;
 const char* password     = STAPSK;
@@ -52,93 +49,63 @@ const char* server_name  = "smartarms.in.ua";
 const char* deviceid     = "2";
 const char* json_light   = "NULL";
 const char* path         = "/light.json";
+char incomingPacket[255];  // buffer for incoming packets
+char replyPacket[] = "Hi there! Got the message :-)";  // a reply string to send back
 uint32_t response = 0;
 const char* type_of_OTA;
 unsigned int server_port = 80;
+unsigned int localUdpPort = 4210;  // local port to listen on
+unsigned long timer;
+unsigned long checkRSSIMillis;
 bool WiFiConnected = false;
-uint32_t    timer_start;
-uint32_t    timer_stop;
-
 //WIFI Settings
 byte modbus_ip[]      = { 192, 168, 1, 126};
 byte modbus_gateway[] = { 192, 168, 1, 1 };
 byte modbus_subnet[]  = { 255, 255, 255, 0 };
-
-/* DynamicJsonDocument initialization */
-DynamicJsonDocument jsonBufferGet(2048);
-/* BME280 initialization */
-Adafruit_BME280 bme;
-/* WiFiClient initialization */
-WiFiClient client;
-/* HTTPClient initialization */
-HTTPClient http;
-
-ESP8266WebServer server(server_port);
-
-ModbusTCPSlave ModbusSlave;
-unsigned long timer;
-unsigned long checkRSSIMillis;
-// Instantiate ModbusMaster object as slave ID 1
-//ModbusMaster232 ModbusNode(1);
 byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-IPAddress dev_ip(0,0,0,0);
 
+DynamicJsonDocument jsonBufferGet(2048);  /* DynamicJsonDocument initialization */
+Adafruit_BME280 bme;                      /* BME280 initialization */
+WiFiClient client;                        /* WiFiClient initialization */
+HTTPClient http;                          /* HTTPClient initialization */
+ESP8266WebServer server(server_port);     /* ESP8266WebServer initialization */
+ModbusTCPSlave ModbusSlave;               /* ModbusTCPSlave initialization */
+WiFiUDP Udp;                              /* WiFiUDP initialization */
+Coap coap(Udp);                           /* Coap initialization */
 
-
-// CoAP client response callback
 void callback_response(CoapPacket &packet, IPAddress ip, int port);
-
-// CoAP server endpoint url callback
 void callback_light(CoapPacket &packet, IPAddress ip, int port);
-
-// UDP and CoAP class
-WiFiUDP Udp;
-Coap coap(Udp);
-//functions modbus
-// node.readHoldingRegisters(,)
-// node.writeSingleRegister(,)
-// node.readInputRegisters(,);
-// node.writeSingleCoil(,)
-// node.readWriteMultipleRegisters(,,,)
-
-void set_timer_start();
-float set_timer_stop();
 void handle_NotFound();
 String SendHTML(float bme_temperature,float bme_humidity);
 byte checkRSSI();
+
 /*---------------------------------------------------------------------------------*/
 /*--------------------------------------SETUP--------------------------------------*/
 /*---------------------------------------------------------------------------------*/
-void setup()
-{
-  int count = 0;
-  /* Init Serial port at 115200 bps*/
+void setup() {
+  int count = 0;            /* Init Serial port at 115200 bps*/
   Serial.begin(115200);     // Initialize Serial port at 115200 bps
-  if (COAP == true)
-  {
-    Serial.println("Setup Callback Light");
+
+  if (COAP == true) {
+    Udp.begin(localUdpPort);  // begin coap server/client
+    Serial.println("[INFO] Setup Callback Light");
     coap.server(callback_light, "light");
-    // client response callback.
-    // this endpoint is single callback.
-    Serial.println("Setup Response Callback");
+    // client response callback. this endpoint is single callback.
+    Serial.println("[INFO] Setup Response Callback");
     coap.response(callback_response);
-    // start coap server/client
-    coap.start();
+    coap.start();             // start coap server/client
   }
-  if (MODBUS232 == true)
-  {
-    //ModbusNode.begin(115200);  // Initialize Modbus communication baud rate
-    Serial.println("Modbus RTU Master Online");
+
+  if (MODBUS232 == true) {
+    Serial.println("[INFO] Modbus RTU Master Online");
       ModbusSlave.begin("Telecom-28778737", "passwordwificasa47893000",
             modbus_ip, modbus_gateway, modbus_subnet);
-
       delay(1000);
       ModbusSlave.MBInputRegister[0] = 100;
       ModbusSlave.MBInputRegister[1] = 65500;
       ModbusSlave.MBInputRegister[2] = 300;
       ModbusSlave.MBInputRegister[3] = 400;
       ModbusSlave.MBInputRegister[4] = 500;
-
       ModbusSlave.MBHoldingRegister[0] = 1;
       ModbusSlave.MBHoldingRegister[1] = 2;
       ModbusSlave.MBHoldingRegister[2] = 3;
@@ -147,36 +114,32 @@ void setup()
   }
   
   while (!Serial) continue; // wait for serial port to connect. Needed for native USB
-  Serial.println("//--------------------------------------------------------------//");
-  Serial.println("//--------------------------------------------------------------//");
-  Serial.println("//--------------------- SMART ARMS SYSTEM ----------------------//");
-  Serial.println("//--------------------------------------------------------------//");
-  Serial.println("//--------------------------- START ----------------------------//");
+  print_cli_start_message();
   
   if (!bme.begin(0x76)) {
     Serial.println("[ERROR] SENSOR BME ERROR! Please check connection!");
     while(1);
   } else {
-    Serial.println("\t[DONE] BME SENSOR is initialise!");
+    Serial.println("[INFO] BME SENSOR is initialise!");
   }
   
   Serial.println("//------------------------ WI-FI START -------------------------//");
   /* Scan Wi-Fi Networks */
   byte numSsid = WiFi.scanNetworks();
-  Serial.println((String)"\tScan Networks\n\tAvailable WiFi discovered: [" + numSsid + "] network(s) found");
+  Serial.println((String)"[INFO] Scan Networks\n\tAvailable WiFi discovered: [" + numSsid + "] network(s) found");
   for (int i = 0; i < numSsid; i++)
   {
-    Serial.println((String)"\t\t[" + (i+1) + "] " + WiFi.SSID(i+1));
+    Serial.println((String)"\t[" + (i+1) + "] " + WiFi.SSID(i+1));
   }
   Serial.println("//------------------------- CONNECTING -------------------------//");
   /* Connect to Wi-Fi */
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("\tPlease be patient. Connecting : ");
+  Serial.print("[INFO] Please be patient. Connecting : ");
   while (WiFi.status() != WL_CONNECTED) {
     delay(SEC);
     Serial.print("*");
-    if ((count%10)==true) Serial.print("\n\tPlease be patient. Connecting : ");
+    if ((count%10)==true) Serial.print("\n[INFO] Please be patient. Connecting : ");
     count++;
   }
   /*ArduinoOTA.setPort(8266);
@@ -206,28 +169,26 @@ void setup()
   ESP_gateway_addr = WiFi.gatewayIP();
   ESP_subnet_addr = WiFi.subnetMask();
   /* print your Sensor ID and server */
-  Serial.println((String)"\n\t[DONE] Connected to : [" + WiFi.status() + "] " + ssid);
-  Serial.println((String)"\t[DONE] WiFi connected sucsses! Time to connection: " + count + "s");
-  Serial.println((String)"\t| x-ESP8266-Sensor ID : "       + deviceid);
-  Serial.println((String)"\t| x-ESP8266-Server    : "       + server_name + ":" + server_port);
-  Serial.println((String)"\t| x-ESP8266-STA-LOCAL-IP:\t"    + ESP_ip_addr.toString());
-  Serial.println((String)"\t| x-ESP8266-STA-GATEWAY:\t"     + ESP_gateway_addr.toString());
-  Serial.println((String)"\t| x-ESP8266-STA-NETMASK:\t"     + ESP_subnet_addr.toString());
-  Serial.println((String)"\t| x-ESP8266-STA-MAC:\t\t"       + WiFi.macAddress());
-  Serial.println((String)"\t| x-ESP8266-AP-MAC:\t\t"        + WiFi.softAPmacAddress());
-  Serial.println((String)"\t| x-ESP8266-sketch-md5:\t\t"    + (String)ESP.getSketchMD5());
-  Serial.println((String)"\t| x-ESP8266-sdk-version:\t"     + (String)ESP.getSdkVersion());
-  Serial.println((String)"\t| x-ESP8266-Core-Version:\t"    + (String)ESP.getCoreVersion() + "-Core-Version");
-  Serial.println((String)"\t| x-ESP8266-free-space:\t\t"    + (String)ESP.getFreeSketchSpace() + "/" + (String)ESP.getSketchSize());
-  Serial.println((String)"\t| x-ESP8266-fragmentation:\t"   + (String)ESP.getHeapFragmentation() + "%");
-  Serial.println((String)"\t| x-ESP8266-Max-Free-Block:\t"  + (String)ESP.getMaxFreeBlockSize() + "RAM");
-  Serial.println((String)"\t| x-ESP8266-Cpu-Freq:\t\t"      + (String)ESP.getCpuFreqMHz() + "MHz");
-  Serial.println((String)"\t| x-ESP8266-reset-reason:\t"    + (String)ESP.getResetReason());
-  Serial.println("\n//-------------------------- CONNECTED -------------------------//");
+  Serial.println((String)"\n┌──[DONE] Connected to : [" + WiFi.status() + "] " + ssid);
+  Serial.println((String)"├──[INFO] WiFi connected sucsses! Time to connection: " + count + "s");
+  Serial.println((String)"└──┬──x-ESP8266-Sensor ID\t:\t"       + deviceid);
+  Serial.println((String)"   ├──x-ESP8266-Server\t:\t"       + server_name + ":" + server_port);
+  Serial.println((String)"   ├──x-ESP8266-STA-LOCAL-IP\t:\t"    + ESP_ip_addr.toString());
+  Serial.println((String)"   ├──x-ESP8266-STA-GATEWAY\t:\t"     + ESP_gateway_addr.toString());
+  Serial.println((String)"   ├──x-ESP8266-STA-NETMASK\t:\t"     + ESP_subnet_addr.toString());
+  Serial.println((String)"   ├──x-ESP8266-STA-MAC\t\t:\t"       + WiFi.macAddress());
+  Serial.println((String)"   ├──x-ESP8266-AP-MAC\t\t:\t"        + WiFi.softAPmacAddress());
+  Serial.println((String)"   ├──x-ESP8266-sketch-md5\t\t:\t"    + (String)ESP.getSketchMD5());
+  Serial.println((String)"   ├──x-ESP8266-sdk-version\t:\t"     + (String)ESP.getSdkVersion());
+  Serial.println((String)"   ├──x-ESP8266-Core-Version\t:\t"    + (String)ESP.getCoreVersion() + "-Core-Version");
+  Serial.println((String)"   ├──x-ESP8266-free-space\t\t:\t"    + (String)ESP.getFreeSketchSpace() + "/" + (String)ESP.getSketchSize());
+  Serial.println((String)"   ├──x-ESP8266-fragmentation\t:\t"   + (String)ESP.getHeapFragmentation() + "%");
+  Serial.println((String)"   ├──x-ESP8266-Max-Free-Block\t:\t"  + (String)ESP.getMaxFreeBlockSize() + "RAM");
+  Serial.println((String)"   ├──x-ESP8266-Cpu-Freq\t\t:\t"      + (String)ESP.getCpuFreqMHz() + "MHz");
+  Serial.println((String)"   └──x-ESP8266-reset-reason\t:\t"    + (String)ESP.getResetReason());
 
   //server.on("/", handle_OnConnect);
   server.onNotFound(handle_NotFound);
-
   server.begin();
 }
 
@@ -237,9 +198,9 @@ void setup()
 void loop() 
 {
   if (COAP == true) {
-    Serial.println("Send Request");
+    Serial.print("[INFO] Send Request: ");
     int msgid = coap.get(IPAddress(0, 0, 0, 0), 5683, "time");
-
+    Serial.println(msgid);
     delay(1000);
     coap.loop();
   }
@@ -283,17 +244,14 @@ void loop()
   String payload = http.getString();    //Get the response payload from server
   delay(1000); // Delay for 1.66min.
   DeserializationError error = deserializeJson(jsonBufferGet, payload);
-  if (error) {
-    Serial.print(F("\t[ERROR] deserializeJson() failed with code "));
-    Serial.println(error.c_str());
-  }
+  if (error) Serial.println((String)"[ERROR] deserializeJson() failed with code :\"" + error.c_str() + "\";");
   /* HTTPClient Read values */
   json_light = jsonBufferGet["user_setting"]["light"];
   /* Make the decision to turn off or on the LED */
   if ((String)json_light == "off") digitalWrite(LED, HIGH);
   else digitalWrite(LED, LOW);
   /* HTTPClient Disconnect */
-  Serial.println((String)"[OK] ResponseCode : [" + httpGetResponseCode + "] Response : \n" + payload + " took: " + set_timer_stop());   //Print return code
+  Serial.println((String)"[OK] ResponseCode : [" + httpGetResponseCode + "] Response : \n" + payload + "[INFO] ResponseTook: " + set_timer_stop() + "s");   //Print return code
   http.useHTTP10(false);
   http.end();
   /* POST DATA to update database */
@@ -326,16 +284,16 @@ void loop()
   if(httpResponseCode>0)
   {
     String response = http.getString(); //Get the response to the request
-    Serial.println((String)"\t ┌──[REQUEST]───[POST]:");
-    Serial.println((String)"\t └──┬──: Temperature:\t"       + bme_temperature + " C");
-    Serial.println((String)"\t    ├──: Humidity:\t\t"        + bme_humidity + " %");
-    Serial.println((String)"\t    ├──: Photo:\t\t"           + light_analog + " LUM");
-    Serial.println((String)"\t    ├──: Pressure:\t\t"        + bme_pressure + " hPa");
-    Serial.println((String)"\t    ├──: Altitude:\t\t"        + bme_altitude + "m");
-    Serial.println((String)"\t    ├──: Signal strength: \t"  + wifi_get_rssi + "dBm");
-    Serial.println((String)"\t    └──: Rellay:\t\t\""        + json_light + "\"");
+    Serial.println((String)"┌──[REQUEST]───[POST]:");
+    Serial.println((String)"└──┬──: Temperature:\t"       + bme_temperature + " C");
+    Serial.println((String)"   ├──: Humidity:\t\t"        + bme_humidity + " %");
+    Serial.println((String)"   ├──: Photo:\t\t"           + light_analog + " LUM");
+    Serial.println((String)"   ├──: Pressure:\t\t"        + bme_pressure + " hPa");
+    Serial.println((String)"   ├──: Altitude:\t\t"        + bme_altitude + "m");
+    Serial.println((String)"   ├──: Signal strength: \t"  + wifi_get_rssi + "dBm");
+    Serial.println((String)"   └──: Rellay:\t\t\""        + json_light + "\"");
     Serial.println("//--------------------------------------------------------------//");
-    Serial.println((String)"[OK] ResponseCode : [" + httpResponseCode + "] Response : " + response + " took: " + set_timer_stop());   //Print return code
+    Serial.println((String)"[INFO] ResponseCode : [" + httpResponseCode + "] Response : " + response + "[INFO] ResponseTook: " + set_timer_stop() + "s");   //Print return code
   } else {
     Serial.println((String)"[ERROR] Error on sending POST: " + httpResponseCode);
   }
@@ -351,21 +309,6 @@ void loop()
 /*------------------------------------------------------------*/
 /*----------------------------FUNC----------------------------*/
 /*------------------------------------------------------------*/
-/* Start timer */
-void set_timer_start() {
-  timer_stop = 0;
-  timer_start = micros();
-}
-/* Stop timer */
-float set_timer_stop() {
-  timer_stop = micros();
-  float time_delta = ((float)timer_stop - (float)timer_start) / 1000;
-  if(time_delta >= 60){
-    return (time_delta/60);
-  } else {
-    return (time_delta);
-  }
-}
 byte checkRSSI() {
   byte quality;
   long rssi = WiFi.RSSI();
@@ -380,19 +323,6 @@ byte checkRSSI() {
   return quality;
 }
 
-/*
-void handle_OnConnect() {
-  float bme_humidity    = bme.readHumidity();
-  float bme_temperature = bme.readTemperature();
-  float bme_pressure    = bme.readPressure() / 100.0F;
-  float bme_altitude    = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  float light_analog    = analogRead(A0);
-  float wifi_get_rssi   = WiFi.RSSI();
-  char* rellay          = "OFF";
-  String s = SendHTML(bme_temperature, bme_humidity);
-  server.send(200, "text/html", s); 
-}
-*/
 void handle_NotFound(){
   server.send(404, "text/plain", "Not founds!");
 }
